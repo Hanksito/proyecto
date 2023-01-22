18e6dedd66dcc11161aef5839e8372a5de670909
Hanksito <barjabalseiro@gmail.com>
2023-01-22 19:38:32 +0100

    preparando videos

--- src/componentes/Layout.js
+++ src/componentes/Layout.js
@@ -37,10 +37,50 @@ const Content = styled.div`
 `;
 
 const PageContainer = styled.div`
+  position: relative;
+  background-size: 50% 50%, 50% 50%;
   margin-top: 10px;
   margin: auto;
   display: flex;
   justify-content: center;
   width: 80%;
   height: 90%;
+  color: #fff;
+  background-color: transparent;
+  overflow: hidden;
+  border-radius: 16px;
+  transition: 1s;
+  animation: rotate 4s linear infinite;
+  @keyframes rotate {
+    0% {
+      border-top: 1px solid rgba(255, 49, 49, 0.5);
+      border-right: 1px solid rgba(0, 255, 255, 0.5);
+      border-bottom: 1px solid rgba(57, 255, 20, 0.5);
+      border-left: 1px solid rgba(255, 255, 113, 0.5);
+    }
+    25% {
+      border-top: 1px solid rgba(255, 255, 113, 0.5);
+      border-right: 1px solid rgba(255, 49, 49, 0.5);
+      border-bottom: 1px solid rgba(0, 255, 255, 0.5);
+      border-left: 1px solid rgba(57, 255, 20, 0.5);
+    }
+    50% {
+      border-top: 1px solid rgba(57, 255, 20, 0.5);
+      border-right: 1px solid rgba(255, 255, 113, 0.5);
+      border-bottom: 1px solid rgba(255, 49, 49, 0.5);
+      border-left: 1px solid rgba(0, 255, 255, 0.5);
+    }
+    75% {
+      border-top: 1px solid rgba(0, 255, 255, 0.5);
+      border-right: 1px solid rgba(57, 255, 20, 0.5);
+      border-bottom: 1px solid rgba(255, 255, 113, 0.5);
+      border-left: 1px solid rgba(255, 49, 49, 0.5);
+    }
+    100% {
+      border-top: 1px solid rgba(255, 49, 49, 0.5);
+      border-right: 1px solid rgba(0, 255, 255, 0.5);
+      border-bottom: 1px solid rgba(57, 255, 20, 0.5);
+      border-left: 1px solid rgba(255, 255, 113, 0.5);
+    }
+  }
 `;
